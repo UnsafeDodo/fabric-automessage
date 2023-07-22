2375d09f334a842339c7cdc1e70e3202ef424b4e
UnsafeDodo <unsafedodo@gmail.com>
2023-07-22 16:45:19 +0200

    Add task send message

--- src/main/java/unsafedodo/fabricautomessage/AutoMessage.java
+++ src/main/java/unsafedodo/fabricautomessage/AutoMessage.java
@@ -1,20 +1,13 @@
 package unsafedodo.fabricautomessage;
 
-import com.mojang.authlib.minecraft.client.MinecraftClient;
-import com.mojang.brigadier.CommandDispatcher;
 import net.fabricmc.api.ModInitializer;
 
-import net.fabricmc.fabric.api.message.v1.ServerMessageEvents;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.command.CommandManager;
-import net.minecraft.server.command.CommandOutput;
-import net.minecraft.server.command.TellRawCommand;
-import net.minecraft.util.Unit;
-import net.minecraft.util.thread.ThreadExecutor;
-import org.intellij.lang.annotations.RegExp;
+import net.minecraft.text.ClickEvent;
+import net.minecraft.text.Text;
 import unsafedodo.fabricautomessage.config.ConfigManager;
 import unsafedodo.fabricautomessage.util.CircularLinkedList;
-import unsafedodo.fabricautomessage.util.CircularListNode;
 import unsafedodo.fabricautomessage.util.Register;
 
 import java.util.concurrent.ScheduledExecutorService;
@@ -27,8 +20,15 @@ public class AutoMessage implements ModInitializer {
 
 	private static String currentMessage;
 
+	private static MinecraftServer server;
+
 	@Override
 	public void onInitialize() {
+
+		ServerLifecycleEvents.SERVER_STARTED.register(minecraftServer -> {
+			server = minecraftServer;
+		});
+
 		if(!ConfigManager.loadConfig())
 			throw new RuntimeException("Could not load config");
 
@@ -36,22 +36,30 @@ public class AutoMessage implements ModInitializer {
 		if(messages.size() > 0){
 			currentMessage = messages.getFirst();
 		}
-		//ScheduledExecutorService executorService = new ScheduledThreadPoolExecutor(1);
-		//executorService.scheduleAtFixedRate(messagePrint, 0, AutoMessage.timeout, TimeUnit.SECONDS);
+
+		ScheduledExecutorService executorService = new ScheduledThreadPoolExecutor(1);
+		executorService.scheduleAtFixedRate(messagePrint, 0, AutoMessage.timeout, TimeUnit.SECONDS);
 	}
 
-	/*Runnable messagePrint = new Runnable() {
+	public static MinecraftServer getServer(){
+		return server;
+	}
+
+	Runnable messagePrint = new Runnable() {
 		@Override
 		public void run() {
 			if(messages.size() > 0){
+				server = getServer();
 				if(currentMessage.matches("(https:\\/\\/www\\.|http:\\/\\/www\\.|https:\\/\\/|http:\\/\\/)?[a-zA-Z]{2,}(\\.[a-zA-Z]{2,})(\\.[a-zA-Z]{2,})?\\/[a-zA-Z0-9]{2,}|((https:\\/\\/www\\.|http:\\/\\/www\\.|https:\\/\\/|http:\\/\\/)?[a-zA-Z]{2,}(\\.[a-zA-Z]{2,})(\\.[a-zA-Z]{2,})?)|(https:\\/\\/www\\.|http:\\/\\/www\\.|https:\\/\\/|http:\\/\\/)?[a-zA-Z0-9]{2,}\\.[a-zA-Z0-9]{2,}\\.[a-zA-Z0-9]{2,}(\\.[a-zA-Z0-9]{2,})? ")){
-
+					server.getPlayerManager().broadcast(Text.literal(currentMessage).styled(style -> style.withClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, "netflix.com"))), true);
 				}
 				else{
-
+					server.getPlayerManager().broadcast(Text.literal(currentMessage), true);
 				}
 				currentMessage = messages.getNextData(currentMessage);
+
+				System.out.println(currentMessage);
 			}
 		}
-	};*/
+	};
 }
